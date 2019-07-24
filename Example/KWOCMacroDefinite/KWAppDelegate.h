3467dfdcee49a2f95fbfcea8449512f4b504a764
GithubXkw1573 <1074030698@qq.com>
2019-07-24 17:39:52 +0800

    Initial commit

---
+++ Example/KWOCMacroDefinite/KWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KWAppDelegate.h
+//  KWOCMacroDefinite
+//
+//  Created by GithubXkw1573 on 07/24/2019.
+//  Copyright (c) 2019 GithubXkw1573. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/KWOCMacroDefinite/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KWOCMacroDefinite
+//
+//  Created by GithubXkw1573 on 07/24/2019.
+//  Copyright (c) 2019 GithubXkw1573. All rights reserved.
+//
+
+@import UIKit;
+#import "KWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KWAppDelegate class]));
+    }
+}
